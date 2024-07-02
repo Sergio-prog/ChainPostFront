6ba30706de67e823a88c7147e86fb3f2ba33b6bd
yuliia_karas <yuliiakaras03@gmail.com>
2024-07-02 21:32:38 +0300

    add leaderboard page

--- src/app/leaderboard/leaderboard.component.ts
+++ src/app/leaderboard/leaderboard.component.ts
@@ -1,10 +1,63 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-leaderboard',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    MatIconModule,
+    MatButtonModule,
+    MatButtonToggleModule,
+  ],
   templateUrl: './leaderboard.component.html',
   styleUrl: './leaderboard.component.scss',
 })
-export class LeaderboardComponent {}
+export class LeaderboardComponent {
+  posts = [
+    {
+      author: {
+        icon: '../../assets/images/person.png',
+        name: 'John Doe',
+      },
+      title: 'Lorem ipsum dolor sit amet',
+      url: '../../assets/images/post.jpg',
+      likes: 150,
+      comments: ['super', 'puper'],
+      shares: 20,
+      liked: false,
+    },
+    {
+      author: {
+        icon: '../../assets/images/person.png',
+        name: 'John Doe',
+      },
+      title: 'Lorem ipsum dolor sit amet',
+      url: '../../assets/images/post.jpg',
+      likes: 150,
+      comments: ['super', 'puper'],
+      shares: 20,
+      liked: false,
+    },
+  ];
+
+  toggleLike(post: any) {
+    post.liked = !post.liked;
+    if (post.liked) {
+      post.likes++;
+    } else {
+      post.likes--;
+    }
+  }
+
+  showComments(post: any) {
+    // Add logic to show comments
+  }
+
+  toggleShare(post: any) {
+    // Add logic to toggle share
+  }
+}
