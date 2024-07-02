daf96e076db566871b70620954f56b02a003f493
yuliia_karas <yuliiakaras03@gmail.com>
2024-07-02 18:50:00 +0300

    add header and navigation

---
+++ src/app/my-stats/my-stats.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-stats',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-stats.component.html',
+  styleUrl: './my-stats.component.scss',
+})
+export class MyStatsComponent {}
